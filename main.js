f0ccf2176196ecf711398a0a5261f2501c0a3290
RoshanShrestha123 <roshan.shrestha4u@gmail.com>
2020-01-29 22:24:54 +0545

    box texture added

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-const scene = new THREE.Scene(); // this is where we put the object , camera and light
-
-// next this we do is create is the camera!!
-// there are two type of camera -> prespective and orthoraphic camera
-
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);//(field of view, aspect ratio, near clipping plane, far clippling plane)
-
-
-//next thing we need is renderer
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene(); 
+let rotationSpeed = 0.01;
+let meshMaterial = 0x00ff00;
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+const renderer = new THREE.WebGLRenderer({antialias : true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// now since we have created the scene so far, lets create the BOX!! WOO HOOO!!.....
-
-//so we need what we called BoxGeometry
-
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+const texture = new THREE.TextureLoader().load('boxImage.jpg');
+const material = new THREE.MeshBasicMaterial({map:texture});
 
 const cube = new THREE.Mesh(geometry,material);
 
@@ -26,8 +18,8 @@ camera.position.z = 5;
 
 function animate(){
     requestAnimationFrame(animate);
-    cube.rotation.x +=0.01;
-    cube.rotation.y +=0.01;
+    cube.rotation.x +=rotationSpeed;
+    cube.rotation.y +=rotationSpeed;
     renderer.render(scene, camera);
 }
 
